89a3da184550e568a69276d90063e1ee7d587f01
Donovan Chavez <chavezdon17@gmail.com>
2021-02-05 22:22:12 -0600

    added files to cache and event listener

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/db.js",
+  "/styles.css",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
